163d3d0166a6be386b002428e6c61f13950337d2
agoetc <b6151@oic.jp>
2018-08-15 22:20:18 +0900

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <button>てすとぼたん</button>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
