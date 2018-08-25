dc4f3a60a43c177a0e2fea313acce7a42500175e
agoetc <b6151@oic.jp>
2018-08-25 23:11:44 +0900

    Todoリスト 作成出来るようにした

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,13 +1,56 @@
 <template>
-  <button>てすとぼたん</button>
+  <div>
+  <form @submit.prevent="addTodo" class="form-inline">
+    <input type="text" v-model="todoText" class="form-control mb-2 mr-sm-2" id="text">
+    <button class="btn btn-primary mb-2">追加</button>
+  </form>
+
+  <table class="table table-sm">
+    <thead>
+    <tr>
+      <th>id</th>
+      <th>Todo</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="todo of todos" :key="todo.id">
+      <th scope="row">{{ todo.id }}</th>
+      <td>{{ todo.text }}</td>
+    </tr>
+    </tbody>
+  </table>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Test',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      todoText: '',
+      todos: []
+    }
+  },
+  mounted () {
+    axios
+      .get('http://localhost:3000/todos')
+      .then(response => { this.todos = response.data })
+      .catch(error => window.alert(error))
+  },
+  methods: {
+    addTodo: function () {
+      if (this.todoText.length > 0) {
+        axios.post('http://localhost:3000/todos', {
+          text: this.todoText
+        }).then(response => {
+          this.todos.push({
+            id: response.data.id,
+            text: response.data.text
+          })
+        })
+      }
     }
   }
 }
